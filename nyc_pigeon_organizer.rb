e73d4d79cf316370419fa4e105bd371d32847c53
Sean Donohue <sdonohue0918@gmail.com>
2020-08-17 17:16:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,18 +4,13 @@ require 'pry'
 
 
 def nyc_pigeon_organizer(data)
+  new_hash = {}
 
   final_results = data.each do |key,value|
     value.each do |inner_key, names|
       names.each do |name|
         binding.pry
-        if name.any? === []
-          final_array[name] = {}
-        end
-        if !final_array[name][key]
-          !final_array[name][key] = []
-        end
-        final_array[name][key].push(inner_key.to_s)
+
       end
     end
   end
