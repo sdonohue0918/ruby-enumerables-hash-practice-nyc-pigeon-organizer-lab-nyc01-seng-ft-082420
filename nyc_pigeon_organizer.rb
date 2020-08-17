db48bddab8078a9757377d9baa9818b9c0d0af3f
Sean Donohue <sdonohue0918@gmail.com>
2020-08-17 18:06:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,12 +4,12 @@ require 'pry'
 
 
 def nyc_pigeon_organizer(data)
-  new_hash = {{[]}}
+  new_hash = {}
 
-  final_results = data.each do |key, value|
+  data.each do |key, value|
     value.each do |inner_key, names|
       names.each do |name|
-
+        new_hash
 
         end
       end
