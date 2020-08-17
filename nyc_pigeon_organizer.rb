4036a94b20390217ac10e792d0c1ff7cf89182fe
Sean Donohue <sdonohue0918@gmail.com>
2020-08-17 15:30:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
 
-  final_results = data.each_with_object({}) do |(key,value), final_array|
+  final_results = data.each do |key,value|
     value.each do |inner_key, names|
       names.each do |name|
 
