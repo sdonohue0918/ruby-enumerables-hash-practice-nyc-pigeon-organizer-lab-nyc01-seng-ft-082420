0f16b2d0c2e26121be7e39109335324b010e5698
Sean Donohue <sdonohue0918@gmail.com>
2020-08-17 17:52:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,12 +9,7 @@ def nyc_pigeon_organizer(data)
   final_results = data.each do |key,value|
     value.each do |inner_key, names|
       value_names_array.each do |name|
-
-
-        binding.pry
-
-
-
+binding.pry
       end
     end
   end
